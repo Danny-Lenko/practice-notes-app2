1f55031cd15441bab68b4f7f8ac8b037d81d8cfd
DannyLenk <danylenko.14@gmail.com>
2022-06-16 08:13:29 +0300

    testing ReactDOM.createRoot

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './style.css';
-import App from './components/App';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './style.css';
+// import App from './components/App';
+//
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+import App from "./components/App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
 
